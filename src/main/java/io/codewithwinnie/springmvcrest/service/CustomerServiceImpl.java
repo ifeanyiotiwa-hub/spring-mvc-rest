a4fade5d67b3964c7274d7c171770b3e5298915e
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-05-02 12:26:09 +0100

    adds Implementation for CustomerService class

---
+++ src/main/java/io/codewithwinnie/springmvcrest/service/CustomerServiceImpl.java
@@ -0,0 +1,39 @@
+package io.codewithwinnie.springmvcrest.service;
+
+import io.codewithwinnie.springmvcrest.domain.Customer;
+import io.codewithwinnie.springmvcrest.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created on 5/2/2022
+ * @author Ifeanyichukwu Otiwa
+ */
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    
+    private final CustomerRepository customerRepository;
+    
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    
+    @Override
+    public Optional<Customer> findCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+    
+    @Override
+    public List<Customer> findAllCustomer() {
+        return customerRepository.findAll();
+    }
+    
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+}
