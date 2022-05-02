36fde48b378316618ee773a592522ee8ff020584
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-05-02 12:25:36 +0100

    adds CustomerService class

---
+++ src/main/java/io/codewithwinnie/springmvcrest/service/CustomerService.java
@@ -0,0 +1,13 @@
+package io.codewithwinnie.springmvcrest.service;
+
+import io.codewithwinnie.springmvcrest.domain.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerService {
+    
+    Optional<Customer> findCustomerById(Long id);
+    List<Customer> findAllCustomer();
+    Customer saveCustomer(Customer customer);
+}
