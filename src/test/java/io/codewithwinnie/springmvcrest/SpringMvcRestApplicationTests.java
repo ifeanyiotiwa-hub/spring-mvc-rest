d8c4a68b2d21337e2f96c1a3a0fb1e17d848f080
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-05-02 02:01:04 +0100

    Initial Commit

---
+++ src/test/java/io/codewithwinnie/springmvcrest/SpringMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codewithwinnie.springmvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
